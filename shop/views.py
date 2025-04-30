5e087ae81210d7808fa1972b7130f53698c43be6
Yurii-Taran50 <alga283052@gmail.com>
2025-04-30 23:16:11 +0300

    modified 4 file

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,5 @@
+from django.core.paginator import Paginator
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
@@ -11,5 +13,27 @@ def index(request):
 
 
 def product_list(request):
-    products = Product.objects.all()
-    return render(request, 'shop/product_list.html', {'products': products})
+    category_id = request.GET.get('category')
+    search_query = request.GET.get('q')
+
+    products = Product.objects.filter(in_stock=True)
+    categories = Category.objects.all()
+
+    if category_id:
+        products = products.filter(category_id=category_id)
+
+    if search_query:
+        products = products.filter(Q(name__icontains=search_query))
+
+    paginator = Paginator(products, 6)  # 6 товаров на страницу
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'page_obj': page_obj,
+        'categories': categories,
+        'selected_category': category_id,
+        'search_query': search_query,
+    }
+
+    return render(request, 'shop/product_list.html', context)
